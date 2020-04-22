7ca21621fa9e7e83be791861aac0787877879bc3
Nicole Wheeler <nwheeler443@gmail.com>
2020-04-22 12:53:35 +0100

    bug fix

--- fastq_pipeline/run_invasiveness_index.sh
+++ fastq_pipeline/run_invasiveness_index.sh
@@ -5,7 +5,7 @@ echo $sample > strain
 echo $sample
 
 bowtie2 --sensitive-local -x topgenes_nt -1 ${sample}_1.fastq.gz -2 ${sample}_2.fastq.gz -p 20 > bowtieout.sam
-samtools view -b -S bowtieout.sam -t topgenes.fai > samtoolsout.bam
+samtools view -b -S bowtieout.sam -t topgenes_nt.fasta.fai > samtoolsout.bam
 samtools sort samtoolsout.bam -o samtoolsout.sort.bam
 samtools index samtoolsout.sort.bam
 
@@ -18,7 +18,7 @@ cat topgenes | while read i; do grep $i samtoolsout.mpileup | grep -P 'DP=' | wc
 awk '$1 < 100 {print NR}' coverage > missinglines
 cat missinglines | while read i; do sed -n ${i}p topgenes >> missinggenes; done
 
-./vcfandfasta_to_edited.pl bcftools.vcf topgenes.fna sample_edited
+./vcfandfasta_to_edited.pl bcftools.vcf topgenes_nt.fasta sample_edited
 translate6frames.sh frames=1 in=sample_edited.fna out=sample.faa overwrite=true
 
 ./trimtruncations.pl sample.faa
@@ -28,5 +28,5 @@ hmmsearch --domtblout hmmsearch models.hmm sample.faa.trim > tmp.txt
 R CMD BATCH --no-save invasiveness_index.R
 cat votes.csv >> votelog
 
-rm hmmsearch hmmsearch.parse sample.faa sample.faa.trim votes.csv missinggenes
+rm hmmsearch hmmsearch.parse sample.faa sample.faa.trim votes.csv missinggenes missing.vcf strain coverage missinglines tmp.txt invasiveness_index.Rout
 rm bowtieout.sam samtoolsout.bam samtoolsout.sort* samtoolsout.mpileup bcftools.bcf bcftools.vcf sample_edited.fna
